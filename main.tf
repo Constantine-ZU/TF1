e10e6546ceccf3ff3a6157d9fc4883d9e382dff0
Constantine-Z <ktb747@gmail.com>
2024-05-01 20:48:51 +0300

    16

--- main.tf
+++ main.tf
@@ -9,20 +9,20 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-variable "TF_VAR_ACCESS_KEY" {
+variable "access_key" {
   description = "AWS Access Key ID"
 }
 
-variable "TF_VAR_SECRET_KEY" {
+variable "secret_key" {
   description = "AWS Secret Access Key"
 }
 
-
 provider "aws" {
-  access_key = var.TF_VAR_ACCESS_KEY
-  secret_key = var.TF_VAR_SECRET_KEY
+  access_key = var.access_key
+  secret_key = var.secret_key
   region     = "eu-north-1"
 }
+
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0c0ec0a3a3a4c34c0"
