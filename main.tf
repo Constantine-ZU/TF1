b4d88bd62e7012b7962bd577a6ce10f86a537ef9
Constantine-Z <ktb747@gmail.com>
2024-05-07 12:05:41 -0700

    2

--- main.tf
+++ main.tf
@@ -117,6 +117,14 @@ resource "aws_security_group" "launch_wizard" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
